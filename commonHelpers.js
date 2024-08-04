f0f0ca78ccc7f1ee2359c58750a5bdb7334343c3
dimacia666 <122976041+dimacia666@users.noreply.github.com>
2024-08-04 20:49:47 +0000

    Deploying to gh-pages from @ dimacia666/goit-js-hw-09@f9b693172270e8de975ceb887f3846468e8dafcd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,10 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=l(e);fetch(e.href,r)}})();const n=document.querySelector(".gallery");function c(o){return o.map(t=>`<li class="gallery-item">
+	                <a class="gallery-link" href=${image.original}>
+		                <img 
+			                class="gallery-image" 
+			                src=${image.preview} 
+			                alt=${image.description} 
+			                />
+	                </a>
+                </li>`)}n.insertAdjacentHTML("beforeend",c);
 //# sourceMappingURL=commonHelpers.js.map
